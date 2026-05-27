d7d3c2fad1bfd29897fa154df23265f5ef6cecb5
dinethkannangara-aus <skfwmngw4r@privaterelay.appleid.com>
2026-05-27 14:35:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/fitness-coach-website-demo/",
+  base: '/fitness-coach-website-demo/',
   plugins: [react()],
-});
+})
